4295d34b8ca44c228097f285588e46ed5714e656
Zach Harris <harris.zach@gmail.com>
2020-03-29 14:45:31 -0500

    Moved Deploy Method from pfdeploymentcontext class to private function

--- powerform/pf-deployment-context/classes/pf-deployment-context.ps1
+++ powerform/pf-deployment-context/classes/pf-deployment-context.ps1
@@ -2,20 +2,4 @@ Class PfDeploymentContext{
     [System.Collections.ArrayList]$Resources=@()
     
     PfDeploymentContext(){}
-
-    [PfResultSet]Deploy(){
-        $resultSet = [PfResultSet]::new()
-
-        Foreach($resource in $this.Resources){
-            $access = (New-Object -TypeName "$($resource.AzResourceType.ClassPrefix)Access" -ArgumentList $resource)
-            $cloudState = $access.GetOrSet()
-
-            $resourceDef = (New-Object -TypeName "$($resource.AzResourceType.ClassPrefix)Definition" -ArgumentList $resource,$cloudState)
-            $resourceDef.BuildState=$resource
-            $resourceDef.CloudState=$cloudState
-            
-            $resultSet.Add($resourceDef)
-        }
-        return $resultSet
-    }
 }
